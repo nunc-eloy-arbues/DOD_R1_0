b00d07c64117964340b2efe114f9d635fcc68f76
nunc-eloy-arbues <eloy.arbues@nunc-solutions.de>
2016-08-16 10:27:21 +0200

    Update proposal.js

--- js/proposal.js
+++ js/proposal.js
@@ -1,6 +1,7 @@
 $urlP = 'http://www.escriba.de/xml/dod_escriba_result_right.xml';
 $urlF = 'http://www.escriba.de/xml/dod_escriba_result_wrong.xml';
 $escUrl = 'http://clarkkent:escriba@192.168.200.172:9080/esi-webgen/api/proposal/compose-sync?d.di_proposal.objectId=a1a3E0000004VneQAE&j.pdfParentId=0063E000001tAINQA2&j.fileName=0063E000001tAINQA2.pdf&c.instanceId=00D3E0000000OR8UAM';
+$urlAWS = 'http://clarkkent:escriba@ec2-52-59-51-10.eu-central-1.compute.amazonaws.com:9080/esi-webgen/api/proposal/compose-async?d.di_proposal.objectId=a1a3E0000004XZYQA2&j.pdfParentId=0063E000002BK9BQAW&j.fileName=Test_NUNC_PROPOSAL_E-level.pdf&c.instanceId=00D3E0000000OR8UAM';
 
 function mainDODR($OpportunityID, $sfSession) {
     $__SFsessionID = $sfSession;
@@ -11,7 +12,7 @@ function mainDODR($OpportunityID, $sfSession) {
     var $Screen4 = "SPECIFICATION OF SERVICES";
     var $Screen5 = "PRODUCTS OFFERED";
     var $Screen6 = "NUNC INDIVIDUAL FIELDS";
-    var $LastScreen = "SUMMARY";
+    var $LastScreen = "SUCCESS!";
 
     /* We use the DTI_ fields only to get information from Workflow. We don't want to show it*/
     $("[id*=DTI_]").css("display", "none");
@@ -36,7 +37,7 @@ function mainDODR($OpportunityID, $sfSession) {
     $("div.waitingSearchDiv").css("display", "none"); //SF change this parameter to show loading gif
 
     //Add this HTML code to the "step line"
-    $("div.pbBody").append("<div id='statsBox'><ul><li class='step1'>1</li><li class='step2'>2</li><li class='step3'>3</li><li class='step4'>4</li><li class='step5'>5</li><li class='step6'>6</li><li class='step7'>7</li></ul></div>");
+    $("div.pbBody").append("<div id='statsBox'><ul><li class='step1'>1</li><li class='step2'>2</li><li class='step3'>3</li><li class='step4'>4</li><li class='step5'>5</li><li class='step6'>6</li></ul></div>");
 
     //Depends of the title we are on a screen or other.
     switch ($("[id*=DT1_TIT]").text().toLowerCase()) {
@@ -74,13 +75,7 @@ function mainDODR($OpportunityID, $sfSession) {
             $("li.step6").addClass("stepCompleted");
             break;
         case $LastScreen.toLowerCase():
-            $("li.step1").addClass("stepCompleted");
-            $("li.step2").addClass("stepCompleted");
-            $("li.step3").addClass("stepCompleted");
-            $("li.step4").addClass("stepCompleted");
-            $("li.step5").addClass("stepCompleted");
-            $("li.step6").addClass("stepCompleted");
-            $("li.step7").addClass("stepCompleted");
+            $("statsBox").remove();
             break;
         default:
     } // Fin Step Line
@@ -100,8 +95,8 @@ function mainDODR($OpportunityID, $sfSession) {
 
         /*Print and Preview buttons*/
 
-        $('<input id="print" type="button" name="btnPrint" value="Generate Document" style="visibility: visible;" class="PrintBtn" onClick="EscribaCall(\'' + $BPMDID + '\',\'' + $OpportunityID + '\');">').appendTo($("[class*=Btn]").parent("td"));
-        $('<input id="print" type="button" name="btnTEST" value="TEST" style="visibility: visible;" class="TestBtn" onClick="EscribaCallT(\'' + $BPMDID + '\',\'' + $OpportunityID + '\');">').appendTo($("[class*=Btn]").parent("td"));
+        $('<input id="print" type="button" name="btnPrint" value="Generate Document" style="visibility: visible;" class="PrintBtn" onClick="EscribaCallT(\'' + $BPMDID + '\',\'' + $OpportunityID + '\');">').appendTo($("[class*=Btn]").parent("td"));
+        // $('<input id="print" type="button" name="btnTEST" value="TEST" style="visibility: visible;" class="TestBtn" onClick="EscribaCallT(\'' + $BPMDID + '\',\'' + $OpportunityID + '\');">').appendTo($("[class*=Btn]").parent("td"));
 
         //$('<input id="preview" type="button" name="btnPreview" value="Preview" style="visibility: visible;" class="PreviewBtn" onClick="window.open(\''+$ESCcall+'\',\'NUNC_Windows\',\'width=600,height=300\')">').appendTo( $("[class*=Btn]").parent("td"));
 
@@ -147,19 +142,21 @@ function EscribaCallT($BpmdId, $OppId) {
         var $DocumentName = getOffertName($BpmdId);
 
         sforce.connection.remoteFunction({
-            url: $urlP,
+            url: $urlAWS,
             requestHeaders: {
                 "Content-Type": "text/xml"
             },
-            dataType: "xml",
             method: "GET",
+            /*requestData: "d.di_proposal.objectId="+$BpmdId+"&j.pdfParentId="+$OppId+"&j.fileName="+$DocumentName+"&c.instanceId=00D3E0000000OR8UAM",*/
             crossDomain: "true",
+
+            dataType: "xml",
             onSuccess: function($response) {
                 CallSuccess($.parseXML($response), $OppId);
             },
             onFailure: function($response) {
-                alert("FAIL: " + $response.textContent);
-            }
+                alert("EscribaCallT - FAIL: " + $response.textContent);
+            },
         });
     } catch (e) {
         alert(e);
@@ -167,23 +164,43 @@ function EscribaCallT($BpmdId, $OppId) {
 }
 
 function CallSuccess($data, $OppId) {
-    var $ESCsuccess = 'OUTPUT';
+    var $ESCsuccess = ['NEWLY_CREATED', 'OUTPUT'];
     var $tagStatus = 'job-status';
 
-    if ($($data).find($tagStatus).text() == $ESCsuccess) {
-        try {
-            var ResQ = sforce.connection.query("SELECT Id FROM Attachment WHERE Parent.Id='" + $OppId + "' ORDER BY CreatedDate DESC NULLS LAST LIMIT 1");
-            var DocId = ResQ.getArray("records")[0].Id;
-
-            window.open("https://" + window.location.hostname + "/servlet/servlet.FileDownload?file=" + DocId, '_parent', '');
-        } catch (e) {
-            alert('ERROR on CallSuccess() doing a Salesforce query. ' +
-                'ERROR: ' + e + '. \nResponse query: ' + ResQ);
-        }
+    if ($.inArray($($data).find($tagStatus).text(), $ESCsuccess) > -1) {
+        showMessage("Document generated successfully", 0);
+        $(".PrintBtn").remove();
     } else {
-        alert('ERROR on CallSuccess() - Status unexpected: ' + $data);
+        showMessage("Oops! Something went wrong", 1);
+        $(".PrintBtn").remove();
     }
 }
+
+function showMessage($msg) {
+    $("div.pbBody").append('<div class="messagePop"><p id="DTO_PopupMsg">' + $msg + '</p><input class="ClosePOPUPBtn" type="button" onClick="closeWindow();" value="CLOSE"></div>');
+    $(".messagePop").css("display", "block");
+}
+
+function showMessage($msg, $n) {
+    var $classList = ['PopupCheck', 'PopupError'];
+
+    if ($n <= $classList.lenght || $n >= 0) {
+        $("div.pbBody").append('<div class="messagePop"><p id="DTO_PopupMsg">' + $msg + '</p><input class="ClosePOPUPBtn" type="button" onClick="closeWindow();" value="CLOSE"></div>');
+        $(".messagePop").css("display", "block");
+        $("#DTO_PopupMsg").addClass($classList[$n]);
+    } else {
+        showMessage($msg);
+    }
+}
+
+
+function getLastDocument($OppId) {
+    var ResQ = sforce.connection.query("SELECT Id FROM Attachment WHERE Parent.Id='" + $OppId + "' ORDER BY CreatedDate DESC NULLS LAST LIMIT 1");
+    var DocId = ResQ.getArray("records")[0].Id;
+
+    window.open("https://" + window.location.hostname + "/servlet/servlet.FileDownload?file=" + DocId, '_parent', '');
+}
+
 
 function closeWindow() {
     window.open('', '_parent', '');
